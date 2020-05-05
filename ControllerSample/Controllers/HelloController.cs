5a65ce53e68288d666eb9bc80b622ee51baf773d
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2020-05-05 08:41:01 -0300

    Add project files.

---
+++ ControllerSample/Controllers/HelloController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerSample.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index(string firstName, string surname)
+        {
+            TimeSpan time = DateTime.Now.TimeOfDay;
+            //var time = new TimeSpan(20, 00, 00);
+            var morning = new TimeSpan(0, 00, 00);
+            var afternoon = new TimeSpan(13, 00, 00);
+            var evening = new TimeSpan(19, 00, 00);
+            var good = "Good ";
+            if (time > morning && time < afternoon)
+            {
+                good += "Morning!";
+            }
+            else if (time > afternoon && time < evening)
+            {
+                good += "Afternoon!";
+            }
+            else {
+                good += "Evening!";
+            }
+            return Content($"{good} {firstName} {surname}! {time} ");
+        }
+        public IActionResult Interation()
+        {
+            return Content("Testing Action.");
+        }
+    }
+}
---
+++ ControllerSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ControllerSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
